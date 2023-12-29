feeb0350c57805da60ae1b78436233fdd0cbbca2
thayzecosta <thayzemartin@gmail.com>
2023-12-29 20:45:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,26 @@
+'''
+Exemplo de uso do framework SCIP para resolver um problema de 
+programação linear inteira mista.
+'''
+
+# pip install pyscipopt
+
+from pyscipopt import Model
+
+model = Model('exemplo')
+
+x = model.addVar('x', vtype='INTEGER')
+y = model.addVar('y')
+
+model.setObjective(x + y, sense='maximize')
+
+model.addCons(-x + 2 * y <= 7)
+model.addCons(2 * x + y <= 14)
+model.addCons(2 * x - y <= 10)
+
+model.optimize()
+
+sol = model.getBestSol()
+
+print('x=', sol[x])
+print('y=', sol[y])
